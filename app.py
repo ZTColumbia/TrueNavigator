a614ccf2f7c8a7fa8c003436dfe5aaf716ee661b
Tong Zhan <tzhan@dyn-129-236-215-42.dyn.columbia.edu>
2022-10-28 14:14:29 -0400

    [add] homepage

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return redirect(url_for('homepage'))
+
+@app.route('/homepage')
+def homepage():  # put application's code here
+    return render_template('homepage.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/homepage.js
@@ -0,0 +1,3 @@
+function startSystem(){
+    console.log("Success!")
+}
