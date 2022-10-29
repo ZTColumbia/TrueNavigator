6d582b8ed7390cc10c206f998ffea3e2dba5024e
Sheng Yu <yu8800388@hotmail.com>
2022-10-28 22:45:00 -0400

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 user_address_info = dict()
 occupation_info = dict()
-
+option_text = ""
 
 @app.route('/')
 def hello_world():
@@ -17,7 +17,7 @@ def homepage():  # put application's code here
 
 @app.route('/options')
 def options():  # put application's code here
-    return render_template('Options.html')
+    return render_template('options.html')
 
 
 @app.route('/toCityAndState', methods=['GET', 'POST'])
@@ -41,6 +41,7 @@ def address_info():
 
     return jsonify(dict(redirect=f'/toOccupation'))
 
+
 @app.route('/toOccupation')
 def render_occupation():
     return render_template('occupation.html')
@@ -51,8 +52,54 @@ def occupation_info():
     global user_address_info
     if request.method == 'POST':
         json_data = request.get_json()
-        occupation_info["occupation"] = json_data["occupation"]
+        occupation_info["occupation"] = json_data['occupation']
         print(occupation_info)
+
+    return jsonify(dict())
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    global option_text
+
+    if request.method == 'GET':
+        option = request.args.get('type')
+        if option == 'amenities':
+            option_text = f"I want to live somewhere near facilities/infrastructures/amenities in {user_address_info['city']} {user_address_info['state']}, Could you give me any suggestions?"
+        elif option == 'education':
+            option_text = f"If I want good education environment for my kids, which neighborhood should I live in {user_address_info['city']} {user_address_info['state']}?"
+        elif option == 'explore':
+            option_text = f"Which neighborhood should people live in {user_address_info['city']} {user_address_info['state']} if they has never been to ?"
+        elif option == 'best_overall':
+            option_text = f"Recommend neighborhoods to live for the best criteria in {user_address_info['city']}{user_address_info['state']}"
+        elif option == 'architecture':
+            return render_template('architecture.html')
+        elif option == 'hobby':
+            return render_template('hobby.html')
+
+
+    print(option_text)
+
+    return
+
+
+@app.route('/results/architecture', methods=['POST'])
+def architecture():
+    global option_text, user_address_info
+    if request.method == 'POST':
+        json_data = request.get_json()
+        session["architecture"] = json_data['architecture']
+        option_text = f"If I am a huge fan of {session['architecture']} style housing, which neighborhood in {user_address_info['city']}{user_address_info['state']} I can consider living in?"
+
+    return jsonify(dict())
+
+
+@app.route('/results/hobby', methods=['POST'])
+def hobby():
+    global option_text, user_address_info
+    if request.method == 'POST':
+        json_data = request.get_json()
+        session["hobby"] = json_data['hobby']
+        option_text = f"Where is the best neighborhood to live if I like {session['hobby']} in {user_address_info['city']}{user_address_info['state']}?"
 
     return jsonify(dict())
 
